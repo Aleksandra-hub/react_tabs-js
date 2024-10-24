2c5d19d9d2d19e5275c1e70e65fece3f86a5d947
Oleksandra Kuziura <radioday2014@gmail.com>
2024-10-24 16:28:37 +0300

    doesnt pass three  tests

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, onTabSelected, activeTabId }) => {
+  return (
+    <ul>
+      {tabs.map(tab => {
+        return (
+          <li
+            className={classNames('', {
+              'is-active': activeTabId === tab.id,
+            })}
+            data-cy="Tab"
+            key={tab.id}
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => {
+                if (activeTabId !== tab.id) {
+                  onTabSelected(tab.content, tab.title, tab.id);
+                }
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
